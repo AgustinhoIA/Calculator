e06bf80c503af7e97be08d5c085031d6ab9fb013
AgustinhoIA <agustinhoigal@gmail.com>
2023-07-11 20:07:09 -0300

    Add btn variables and eventlistener lines, fix index.html

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const btn1 = document.querySelector("number 1")
+const btn2 = document.querySelector("number 2")
+const btn3 = document.querySelector("number 3")
+const btn4 = document.querySelector("number 4")
+const btn5 = document.querySelector("number 5")
+const btn6 = document.querySelector("number 6")
+const btn7 = document.querySelector("number 7")
+const btn8 = document.querySelector("number 8")
+const btn9 = document.querySelector("number 9")
+const btn0 = document.querySelector("number 0")
+const plus = document.querySelector("sum")
+const minus = document.querySelector("minus")
+const multi = document.querySelector("multiply")
+const divide = document.querySelector("divide")
+const equal = document.querySelector("equal")
+const erase = document.querySelector("erase")
+const dot = document.querySelector("dot")
+const clear = document.querySelector("clear")
+
+
 const add = function(number1, number2) {
     return number1 + number2;
   };
@@ -20,4 +40,21 @@ const add = function(number1, number2) {
     } return result;
   };
 
-  
+  btn1.addEventListener('click',function(){})
+  btn2.addEventListener('click',function(){})
+  btn3.addEventListener('click',function(){})
+  btn4.addEventListener('click',function(){})
+  btn5.addEventListener('click',function(){})
+  btn6.addEventListener('click',function(){})
+  btn7.addEventListener('click',function(){})
+  btn8.addEventListener('click',function(){})
+  btn9.addEventListener('click',function(){})
+  btn0.addEventListener('click',function(){})
+  dot.addEventListener('click',function(){})
+  plus.addEventListener('click',function(){})
+  minus.addEventListener('click',function(){})
+  multi.addEventListener('click',function(){})
+  divide.addEventListener('click',function(){})
+  equal.addEventListener('click',function(){})
+  erase.addEventListener('click',function(){})
+  clear.addEventListener('click',function(){})
