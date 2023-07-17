f17404e399845191956606bcf67505c6bd4758fe
AgustinhoIA <agustinhoigal@gmail.com>
2023-07-16 21:54:18 -0300

    Add proper addevent and variable storage to numbers

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const numberButton = document.querySelectorAll("number")
+const numberButton = document.querySelectorAll(".number")
 
-const plus = document.querySelector("sum")
-const minus = document.querySelector("minus")
-const multi = document.querySelector("multiply")
-const divi = document.querySelector("divide")
-const equal = document.querySelector("equal")
-const erase = document.querySelector("erase")
-const dot = document.querySelector("dot")
-const clear = document.querySelector("clear")
+const plus = document.querySelector(".sum")
+const minus = document.querySelector(".substract")
+const multi = document.querySelector(".multiply")
+const divi = document.querySelector(".divide")
+const equal = document.querySelector(".equal")
+const erase = document.querySelector(".erase")
+const dot = document.querySelector(".dot")
+const clear = document.querySelector(".clear")
 
-let number1 = 0
-let number2 = 0
-let operator = Any
+let number1 = ""
+let number2 = "0"
+let operator = ""
 
 function operate(number1, number2, operator) {
   switch(operator) {
-    case "+":
+    case " + ":
       result = add(number1, number2)
       break
-    case '-':
+    case ' - ':
       result = subtract(number1, number2)
       break
-    case 'X':
+    case ' X ':
       result = multiply(number1, number2)
-    case '÷':
+    case ' ÷ ':
       result = divide(number1, number2)
       break
     default:
@@ -48,11 +48,23 @@ function divide(number1, number2) {
 }
 
 
-
+function getNumber() {
 numberButton.forEach(button => {
-  button.addEventListener('click',function(){})
+  button.addEventListener('click',function(){
+    let numberToDisplay = button.querySelector("p").textContent
+    number1 = number1 + `${numberToDisplay}`
+    console.log(Number(number1))
+  })
 })
+console.log(numberButton)
+}
 
+/* next step is making a switch case into GetNumber in witch if a operetor has been imputed 
+then any new number get puts into number 2, to do that any operator has to switch a variable to indicate that
+ in principle the result in the display should show the numbers that you imput into then, the operator and the second number
+ when you press equal that display should be replaced with the matematical result of the operation, and once you press any
+ other number then it gets reseted to showing the current imput*/
+ 
 dot.addEventListener('click',function(){})
 plus.addEventListener('click',function(){})
 minus.addEventListener('click',function(){})
@@ -61,3 +73,5 @@ divi.addEventListener('click',function(){})
 equal.addEventListener('click',function(){})
 erase.addEventListener('click',function(){})
 clear.addEventListener('click',function(){})
+
+getNumber()
